e4b672d3444527e2bcb219364e5bd2cb0f1a1158
Toomore Chiang <toomore0929@gmail.com>
2014-06-26 03:25:06 +0800

    Add gae user api.

--- hello.go
+++ hello.go
@@ -2,11 +2,27 @@ package hello
 
 import "fmt"
 import "net/http"
+import "appengine"
+import "appengine/user"
 
 func init() {
     http.HandleFunc("/", handle)
 }
 
 func handle(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Hello World!")
+    c := appengine.NewContext(r)
+    u := user.Current(c)
+
+    if u == nil {
+        url, err := user.LoginURL(c, r.URL.String())
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+        w.Header().Set("Location", url)
+        w.WriteHeader(http.StatusFound)
+        return
+    }
+
+    fmt.Fprintf(w, "Hello, %v", u)
 }
